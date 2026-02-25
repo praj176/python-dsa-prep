0e0ba5741b3d8a33b1ee10a3ef9c59c39a5b4e1d
Prajwal B <98519010+praj176@users.noreply.github.com>
2026-02-25 18:12:01 +0530

    Created function to make tea

--- day_00/programming_basics/00_non_python_code.py
+++ day_00/programming_basics/00_non_python_code.py
@@ -1,1 +1,14 @@
-
+def make_tea():
+    if not kettle_has_water():
+        fill_kettle()
+    plug_in_kettle()
+    boil_water()
+    if not is_cup_clean():
+        wash_cups
+    add_to_cup("tea_leaves")
+    add_to_cup("sugar")
+    pour("boiled_water")
+    stir("tea")
+    serve("tea")
+    
+make_tea()
